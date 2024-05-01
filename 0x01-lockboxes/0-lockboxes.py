a73f6fe44be9dd03b7ac7cceb05dc44f77dccafe
Onuoha nnaemeka <markonuoha6@gmail.com>
2024-05-01 22:55:19 +0100

    lockboxes running correct output

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!usr/bin/python3
+"""
+0-lockboxes.py
+"""
+
+
+def canUnlockAll(boxes):
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+    queue = [0]
+
+    while queue:
+        current_box = queue.pop(0)
+        for key in boxes[current_box]:
+            if 0 <= key < n and not visited[key]:
+                visited[key] = True
+                queue.append(key)
+
+    return all(visited)
